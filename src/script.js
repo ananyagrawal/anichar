9dccb68c8e87b38ce1dd2ed5169e83476e7a9350
Anany Agrawal <ananyagrawal698@gmail.com>
2022-12-02 23:03:59 +0530

    Added search function

--- src/script.js
+++ src/script.js
@@ -1,12 +1,31 @@
 
+const BASE_URL = `https://kitsu.io/api/edge`;
+const newAniBtn = document.getElementById('btn');
+const animageDiv = document.getElementById('animage');
+const searchButton = document.getElementById('searchBtn');
+const searchInput = document.getElementById('search-bar');
+
 const getAnimeChar = (id) => {
-    fetch(`https://kitsu.io/api/edge/anime/${id}`)
+    fetch(`${BASE_URL}/anime/${id}`)
     .then(response => response.json())
     .then(json => {
-        document.querySelector('body').innerHTML += `<img src="${json.data.attributes.posterImage.small}" height=200 width=200/>`
+        animageDiv.innerHTML = `<img src="${json.data.attributes.posterImage.small}" height=200 width=200/>`
     })
 }
 
-const id=Math.random()*100;
+const getSearchAnime = (name) =>{
+    fetch(`${BASE_URL}/anime?filter[text]=${name}`)
+    .then(response => response.json())
+    .then(json => {
+        const anime = json.data[0];
+        animageDiv.innerHTML = `<img src="${anime.attributes.posterImage.small}" height=200 width=200/>`
+    })
+}
 
-getAnimeChar(id)
+// const id=Math.random()*100;
+const randomHero = () => {
+    return Math.floor(Math.random() * 12500)
+}
+
+newAniBtn.onclick = () => getAnimeChar(randomHero());
+searchButton.onclick =  () => getSearchAnime(searchInput.value);
